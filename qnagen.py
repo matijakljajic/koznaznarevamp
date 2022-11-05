4d03bccbfca4261ff2fa3844b290298f435eafa2
Matija Kljajić <matijakljajic173@gmail.com>
2022-11-05 21:06:49 +0100

    Create splash/main

--- qnagen.py
+++ qnagen.py
@@ -8,7 +8,7 @@ Q&A generator using Pandas and Random.
 import pandas
 import random
 
-qna = pandas.read_excel("izvucena_pitanja.xlsx", "sheet", usecols = "A,B")
+qna = pandas.read_excel("resources/izvucena_pitanja.xlsx", "sheet", usecols = "A,B")
 qnad = qna.to_dict('index')
 
 while True:
